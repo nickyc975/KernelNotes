1f292be2a8472b847fd252b24675a5dbc93dff0e
Nicky Chen <chenjinlong2016@outlook.com>
2020-12-03 16:26:36 +0800

    Using pure syscall io api.

--- code/fstest.c
+++ code/fstest.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 20
+#define FILE_NAME "test.txt"
+#define OPEN_FAILED_MSG(name) "Open file " name " failed\n"
 
-const char *FILE_NAME = "test.txt";
-
-int main() {
-    FILE *file = fopen(FILE_NAME, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Open file %s failed: ", FILE_NAME);
-        perror("");
+int main()
+{
+    char buff[BUFFER_SIZE] = {0};
+    int fd = open(FILE_NAME, O_RDONLY);
+    if (fd < 0)
+    {
+        const char *msg = OPEN_FAILED_MSG(FILE_NAME);
+        write(STDOUT_FILENO, msg, strlen(msg));
         return 0;
     }
 
-    char content[BUFFER_SIZE] = {0};
-    size_t nread = fread(content, sizeof(char), BUFFER_SIZE, file);
-    printf("%s", content);
-    fclose(file);
+    ssize_t nread = read(fd, buff, BUFFER_SIZE);
+    write(STDOUT_FILENO, buff, nread);
+    close(fd);
     return 0;
 }
