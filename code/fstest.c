2b73d6b485f5bac4b4950a9a55e635e8412da378
Nicky Chen <chenjinlong2016@outlook.com>
2020-12-07 21:08:17 +0800

    Using pure syscall.

--- code/fstest.c
+++ code/fstest.c
@@ -1,24 +1,25 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 
 #define BUFFER_SIZE 20
 #define FILE_NAME "test.txt"
-#define OPEN_FAILED_MSG(name) "Open file " name " failed\n"
+#define OPEN_FAILED_MSG(name) "Open file " name " failed!\n"
 
 int main()
 {
     char buff[BUFFER_SIZE] = {0};
-    int fd = open(FILE_NAME, O_RDONLY);
+    int fd = syscall(SYS_open, FILE_NAME, O_RDONLY);
     if (fd < 0)
     {
         const char *msg = OPEN_FAILED_MSG(FILE_NAME);
-        write(STDOUT_FILENO, msg, strlen(msg));
+        syscall(SYS_write, STDOUT_FILENO, msg, strlen(msg));
         return 0;
     }
 
-    ssize_t nread = read(fd, buff, BUFFER_SIZE);
-    write(STDOUT_FILENO, buff, nread);
-    close(fd);
+    ssize_t nread = syscall(SYS_read, fd, buff, BUFFER_SIZE);
+    syscall(SYS_write, STDOUT_FILENO, buff, nread);
+    syscall(SYS_close, fd);
     return 0;
 }
